2fcd034bcc8a301de8835b19345012df25bbc016
Eric <pturnavanguard@gmail.com>
2021-03-28 13:35:18 +0300

    alsmost there

---
+++ iot/lib/Realtime.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+import 'CircleProgress.dart';
+import 'main.dart';
+
+class Realtime extends StatefulWidget {
+  @override
+  _RealtimeState createState() => _RealtimeState();
+}
